93a1d9990009dc97d05330ab1fd7b63ceefb83c6
ajikamaludin <aji19kamaludin@gmail.com>
2016-09-02 21:15:57 +0700

    hapus function

--- function/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $host = 'localhost';
-  $user = 'root';
-  $pass = 'face';
-  $database = 'sekolah';
-
-$koneksi = mysqli_connect($host,$user,$pass,$database) or die(mysqli_error());
-mysqli_close();
-?>
